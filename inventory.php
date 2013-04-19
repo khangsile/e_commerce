0eae6da3eca3013ce98ea35fcac33679af3c8cc9
Jason Hettmansperger <jjbeanz@Jasons-MacBook-Pro.local>
2013-04-19 17:01:40 -0400

    Created just the page for inventory.

---
+++ inventory.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+?>
+
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <table>
+                <tr><td>
+                    <div class="link"><a href="home.php">Home</a></div>
+                </td></tr>
+                <tr><td>
+                    <h3>Inventory Management. </h3>
+                    <h3>Staff user: <?php echo $_SESSION['username']?>!</h3>
+                </td></tr>
+                <tr><td>
+                    <h4>Inventory Update:</h4>            
+                </td></tr>
+            </table>
+            <form name="login" method="post" action="inventory.php">
+                <table>
+                    <tr>
+                    <td>Item ID :</td>
+                    <td><input name="username" type="text" id="username"></input></td>
+                    </tr>
+                    <tr>
+                    <td>Updated Quantity :</td>
+                    <td><input name="password" type="text" id="password"></input></td>
+                    </tr>
+                    <tr>
+                    <td><input name="submit" type="submit" id="submit"></input></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        <pre>
+        <?php
+        include 'DatabaseConnector.php';
+        $dbconnector = new DatabaseConnector();
+        $dbconnector->open();
+        $all_items = $dbconnector->get_all_Items();
+        
+        for($counter = 0; $counter< count($all_items); $counter++) {
+            $item_title = $all_items[$counter]["title"];
+            $item_id = $all_items[$counter]["item_id"];
+            $item_description = $all_items[$counter]["item_description"];
+            $item_price = $all_items[$counter]["item_price"];
+            
+            echo '<br/>';
+            echo 'Item: '; echo $item_title; echo'<br/>';
+            echo 'Item ID: '; echo $item_id; echo'<br/>';
+            echo 'Item Description: '; echo $item_description; echo '<br/>';
+            echo 'Item Price: '; echo $item_price; echo'<br/>';
+        }
+        
+        ?>
+        </pre>
+        
+        
+        <pre>
+        <?php
+        var_dump($all_items);
+        
+        ?>
+        </pre>
+    </body>
+</html>
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
